e53203b529af109c4d4328e43d6fd1c5b9f8b0d4
Jose Angel Hernandez Radilla <jhernandezr1313@alumno.ipn.mx>
2020-10-17 16:13:48 -0600

    Updates

---
+++ precache-manifest.1da1f9a5cdf913f1ee9b4f1318e14516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec9b27928b7948730dbffd323b0a916",
+    "url": "/karuta/index.html"
+  },
+  {
+    "revision": "4de145d4a9c4f0609395",
+    "url": "/karuta/static/css/main.1975e12c.chunk.css"
+  },
+  {
+    "revision": "a071d3e777bd9ba5c2bb",
+    "url": "/karuta/static/js/2.14a53f1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karuta/static/js/2.14a53f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de145d4a9c4f0609395",
+    "url": "/karuta/static/js/main.01fec951.chunk.js"
+  },
+  {
+    "revision": "4eea17318e45530492f3",
+    "url": "/karuta/static/js/runtime-main.eb58603e.js"
+  },
+  {
+    "revision": "bde33409ab567611d8ce31e79e451eef",
+    "url": "/karuta/static/media/61.bde33409.png"
+  }
+]);
